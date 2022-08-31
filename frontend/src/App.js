5539530d5dc6a82d42135e8342fb868df821fa05
PaStar98 <startekp98@gmail.com>
2022-08-31 14:52:11 +0200

    Frontend starting template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-import image from './images/piwko.png'
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={image} className="App-logo" alt="logo" />
-        <p>
-          Siema Siema o tej porze kazydy wypić może
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+  return <h1>Hello CRUD</h1>
 }
-
-export default App;
